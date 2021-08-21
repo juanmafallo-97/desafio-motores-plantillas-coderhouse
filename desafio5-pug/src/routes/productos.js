60a6c479d577f74eb29b73b45310942a1a362f70
juanmafallo-97 <juanmafallo@gmail.com>
2021-08-21 12:45:59 -0300

    Eliminadas rutas innecesarias

--- desafio5-pug/src/routes/productos.js
+++ desafio5-pug/src/routes/productos.js
@@ -15,43 +15,11 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/:id", async (req, res) => {
-  try {
-    const id = Number(req.params.id);
-    const producto = await api.getById(id);
-    res.status(201).json(producto);
-  } catch (error) {
-    res.send({ error: error.message });
-  }
-});
-
 router.post("/", async (req, res) => {
   try {
     const newProduct = req.body;
     const savedProduct = await api.save(newProduct);
     res.json(savedProduct);
-  } catch (error) {
-    res.json({ error: error.message });
-  }
-});
-
-router.put("/:id", async (req, res) => {
-  try {
-    const id = Number(req.params.id);
-    const product = req.body;
-    await api.updateProduct(id, product);
-    const updatedProduct = await api.getById(id);
-    res.status(201).json(updatedProduct);
-  } catch (error) {
-    res.json({ error: error.message });
-  }
-});
-
-router.delete("/:id", async (req, res) => {
-  try {
-    const id = Number(req.params.id);
-    await api.deleteById(id);
-    res.json({ success: `Product with id ${id} deleted` });
   } catch (error) {
     res.json({ error: error.message });
   }
